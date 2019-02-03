155c53e828763fb85d293d2325e235a1e74ec0dc
Regi Oommen <o_regi@hotmail.com>
2019-02-03 12:07:22 -0800

    changing golden script

--- goldenpath.sh
+++ goldenpath.sh
@@ -1,15 +1,22 @@
-echo 'This is my' $Server
+#!/bin/bash
+
+echo 'This is my server:' $Server
 echo 'This is my gateway type' $gateway_service_type
 echo 'This is my YAMLPath' $YAMLPath
+echo 'This is value of WSDLPath' $WSDLPath
 
 
 
-echo 'Reading file inside GoodVeloxAPI dir' 
-cat $YAMLPath/test.yaml
+
+if [[ $YAMLPath == "GoodVeloxAPI" ]] && [[$WSDLPath == "GoodVeloxAPI/SoapWSDL" ]]
+then
+   echo 'Reading file inside GoodVeloxAPI dir' 
+   cat $YAMLPath/test.yaml
+   echo '+++++++++++Reading wsdl file in subdir+++++++++++++++++++++'
+   cat $WSDLPath/anotherfile.wsdl
+fi
 
 
-echo '+++++++++++Reading wsdl file in subdir+++++++++++++++++++++'
-cat $WSDLPath/anotherfile.wsdl
 
 
 
