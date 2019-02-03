466010ec32dd7b66190c6b09e6bd135c72b324bd
Regi Oommen <o_regi@hotmail.com>
2019-02-03 10:58:46 -0800

    adding new params to golden script

--- goldenpath.sh
+++ goldenpath.sh
@@ -1,5 +1,8 @@
-echo 'This is my' $Bloat
-echo 'This is my path' $Football
+echo 'This is my' $Server
+echo 'This is my gateway type' $gateway_service_type
+echo 'This is my YAMLPath' $YAMLPath
+
+
 
 
 
