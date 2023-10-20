af30b3a9634f18a8a4f2c399e3560801b7734b58
Regi Oommen <regi.oommen@ibm.com>
2023-10-19 22:00:58 -0400

    new lib file

---
+++ libs/common.groovy
@@ -0,0 +1,13 @@
+
+
+def run_test(){
+    try {
+        echo "The value of DEPLOY is deploy"
+    } catch (error) {
+        echo "Suite:  -- test failed"
+        return error
+    } finally {
+        echo "From Finally in lib method"
+        // junit '*.xml'
+    }
+}
