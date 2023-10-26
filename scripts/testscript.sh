95191d78873591482c931fb7c8334aa74573e13c
Regi Oommen <regi.oommen@ibm.com>
2023-10-26 16:52:02 -0400

    test

--- scripts/testscript.sh
+++ scripts/testscript.sh
@@ -5,7 +5,9 @@
 check_subsys_status ()
 {
    echo "Hello inside check subsys status method"
-
+   exit 1
 }
 echo 'Hello from inside main script'
 check_subsys_status
+
+echo 'After exit in function call above'
