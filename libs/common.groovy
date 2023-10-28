7ef853ae1ea86adeb1433fe6d841b051f58f184c
Regi Oommen <regi.oommen@ibm.com>
2023-10-27 23:55:59 -0400

    error added

--- libs/common.groovy
+++ libs/common.groovy
@@ -16,7 +16,7 @@ def run_test(){
             throw new Exception ("$cause")
         } else {
             cause = "Runtime error"
-            unstable('Test Failure')
+            error('Test Failure')
         }
         
     } finally {
